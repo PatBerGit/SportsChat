d81ea1c6037c8b2d240257ff378b46e293c2ee12
Patrik Bergsängel <patrik.bergsangel@capgemini.com>
2023-03-22 16:08:13 +0100

    Added header and footer components

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './footer.css';
+const Footer = () => (
+  <div className="footer">
+    <p className="contactUs">Kontakta oss</p>
+    <p className="email"><a href="mailto:info@sportschat.se">info@sportschat.se</a></p>
+  </div>
+)
+export default Footer;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './header.css';
+//import logoUrl from '../../assets/logo.svg';
+const Header = () => (
+  <div className="header">
+    
+  </div>
+)
+export default Header;
