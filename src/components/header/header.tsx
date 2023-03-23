d2d10b74080dcebb909cba2b4fc6d6c23b0041af
Patrik Bergsängel <patrik.bergsangel@capgemini.com>
2023-03-23 11:29:17 +0100

    Implemented hamburger menu

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import './header.css';
+import { createGlobalStyle } from "styled-components";
+import Menu from "../Menu/Menu";
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    min-height: 100vh;
+    width: 100%;
+    background-color: #fdf2e9;
+    font-family: 'Annie Use Your Telescope', cursive;
+  }
+`;
+
 const Header = () => (
+  
   <div className="header">
+    <GlobalStyle />
+    <Menu />
     <p className="title">Sports chat</p>
   </div>
 )
