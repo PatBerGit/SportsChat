6839cd9877a76c9a43c74b5ec2c2b1c9f595fbea
Patrik Bergsängel <patrik.bergsangel@capgemini.com>
2023-03-24 16:38:40 +0100

    Button component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import Badge from './components/Badge/Badge';
+import Button from './components/Button/Button';
+import { PhoneIcon } from '@heroicons/react/24/solid'
 
 function App() {
   return (
@@ -11,6 +13,14 @@ function App() {
         <Badge
           text="Fotboll"
           filled={false}
+        />
+        <Button 
+          text="Button"
+          href='#'
+          type="primary"
+          filled={false}
+          icon={<PhoneIcon />}
+
         />
       </div>
         <Footer />
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,11 @@ export interface BadgeInterface{
     text: string;
     filled?: boolean;
 }
+
+export interface ButtonInterface{
+    text: string;
+    filled?: boolean;
+    type: string;
+    href: string;
+    icon?: JSX.Element;
+}
